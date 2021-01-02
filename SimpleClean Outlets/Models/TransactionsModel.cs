22056119b24f9ac7e87f9e6261904b9d8833d52d
Arfan yoga <ayogaan@gmail.com>
2021-01-02 08:29:28 -0500

    update payment bug

--- SimpleClean Outlets/Models/TransactionsModel.cs
+++ SimpleClean Outlets/Models/TransactionsModel.cs
@@ -44,7 +44,7 @@ namespace SimpleClean_Outlets.Models
         }
 
         public void GetOmzet() {
-            query = "select sum(orders.total_harga) as [total] from invoice join orders on orders.id = invoice.id_order  where orders.id_outlets =" + AccountModel.OutletsId;
+            query = "select sum(orders.total_harga) as [total] from invoice join orders on orders.id = invoice.id_order  where invoice.status='done' and orders.id_outlets =" + AccountModel.OutletsId;
             openConnSql();
             sqlConnection.Open();
             using (command = new SqlCommand(query, sqlConnection))
